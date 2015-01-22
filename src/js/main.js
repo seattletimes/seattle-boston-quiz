695fdad86e2980be5d8e8165b3eccd0c8eec322a
Audrey Carlsen <acarlsen@seattletimes.com>
2015-01-21 17:49:41 -0800

    GO HAWKS

---
+++ src/js/main.js
@@ -0,0 +1,88 @@
+//Use CommonJS style via browserify to load other modules
+
+var $ = require("jquery");
+var ich = require("icanhaz");
+var questionTemplate = require("./_questionTemplate.html");
+var resultTemplate = require("./_resultTemplate.html");
+
+var score = 0;
+var questionIndex;
+var id = 1;
+
+// Set up templates
+ich.addTemplate("questionTemplate", questionTemplate);
+ich.addTemplate("resultTemplate", resultTemplate);
+
+var Share = require("share");
+new Share(".share-button", {
+  ui: {
+    flyout: "bottom left"
+  }
+});
+
+var showQuestion = function(questionId) {
+  $(".index").html(id + " of " + Object.keys(window.quizData).length);
+  $(".question-box").html(ich.questionTemplate(window.quizData[id]));
+};
+
+var watchInput = function() {
+// show next button when answer is selected
+  $(".quiz-box").on("click", "input", (function(){
+    $(".next").addClass("active");
+    $(".next").attr("disabled", false);
+  }));
+};
+
+var watchNext = function() {
+  $(".next").click(function() {
+    // score answer
+    var correct = $("input:checked").val();
+    if (correct) {
+      score += 1;
+    }
+
+    // move on to next question
+    if (id < Object.keys(window.quizData).length) {
+      id += 1;
+      showQuestion(id);
+      $(".next").removeClass("active");
+      $(".next").attr("disabled", true);
+      // Change button text on last question
+      if (id == Object.keys(window.quizData).length) {
+        $(".next").html("FINISH");
+      }
+    } else {
+      calculateResult();
+    }
+  });
+};
+
+var calculateResult = function() {
+  for (var index in resultsData) {
+    var result = resultsData[index];
+    if (score >= result.min && score <= result.max) {
+      // display result
+      result.score = score;
+      $(".quiz-box").html(ich.resultTemplate(result));
+    }
+  }
+
+  $(".retake").removeClass("hidden");
+  $(".quiz-container").addClass("results");
+  new Share(".share-button", {
+    // description: "I got " + result.player + "! Which Seahawk are YOU?",
+    // image: result.image,
+    // ui: {
+    //   flyout: "bottom left",
+    //   button_text: "SHARE RESULTS"
+    // },
+    // facebook: {
+    //   caption: "I got " + result.player + "! Which Seahawk are YOU?"
+    // }
+  });
+  $(".share-button").addClass("share-results");
+};
+
+showQuestion(questionIndex);
+watchInput();
+watchNext();
